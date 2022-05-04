7ed92a6c604e84e2cf32bccec780388879a6adb5
pierreberranger <pierre@berranger.eu>
2022-05-04 18:55:57 +0200

    version avec tout sauf le design, code sale

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, make_response, render_template, request
 from flask_socketio import SocketIO, join_room, leave_room
 from numpy import broadcast
 from game_backend import game
@@ -7,16 +7,19 @@ from game_backend.game_index import *
 app = Flask(__name__)
 socketio = SocketIO(app)
 game_index = GameIndex()
+player_index = dict()
 game = Game(hidden_monsters=True)
 
 @app.route("/play")
 def play():
     game_id = request.args.get("game_id", default=None)
     multi = request.args.get("multi") == "1"
-    if game_id == None:
+    if game_id==None and (request.cookies.get('game_id')==None or multi):
         game_id = generateId(game_index)
         game = Game(multiplayer=multi)
         game_index[game_id] = game
+    elif not(multi): 
+        game_id = request.cookies.get('game_id')
     if not (game_id in game_index.keys()):
         return render_template("unexisting_id.html")
     
@@ -38,21 +41,43 @@ def welcome():
 @socketio.on("join")
 def on_join_msg(json):
     room = json["room"]
-    player_id = json["id"]
+    socket_id = json["id"]
+    player_id = json["player_id"]
     game = game_index[room]
-    print(f"received new join message from {player_id}")
+    player_index[player_id] = socket_id
+    print(f"received new join message from {socket_id}")
     join_room(room)
     if not(player_id in game.getPlayers().keys()):
         data = game.addPlayer(player_id)
-        socketio.emit("you_joined", {"lifes": game.getPlayers()[player_id].getLife()}, to=player_id)
+        socketio.emit("you_joined", {"lifes": game.getPlayers()[player_id].getLife()}, to=player_index[player_id])
         socketio.emit("new_user", {"data": data}, room=room)
     else:
-        socketio.emit("you have already joined", to=player_id)
+        socketio.emit("you have already joined", to=player_index[player_id])
+
+
+@socketio.on("get a saved game")
+def save_msg(json):
+    socket_id = json["id"]
+    player_id = json["player_id"]
+    room = json["room"]
+    join_room(room)
+    game = game_index[room]
+    try:
+        check_id(player_id, socket_id)
+    except KeyError :
+        player_index[player_id] = socket_id
+    if len(game.getPlayers())==0:
+        data = game.addPlayer(player_id)
+        socketio.emit("new_user", {"data": data}, room=room)
+        player_index[player_id] = socket_id
+    socketio.emit("you_joined", {"lifes": game.getPlayers()[player_id].getLife()}, to=player_index[player_id])
 
 @socketio.on("move")
 def on_move_msg(json, methods=["GET", "POST"]):
     room = json["room"]
-    player_id = json["id"]
+    socket_id = json["id"]
+    player_id = json["player_id"]
+    check_id(player_id, socket_id)
     game = game_index[room]
     print(f"received move ws message from {player_id}")
     dx = json['dx']
@@ -64,15 +89,17 @@ def on_move_msg(json, methods=["GET", "POST"]):
 @socketio.on("is_hit?")
 def on_is_hit_msg(json):
     room = json["room"]
-    player_id = json["id"]
+    socket_id = json["id"]
+    player_id = json["player_id"]
+    check_id(player_id, socket_id)
     game = game_index[room]
     print(f"received is_hit? ws message from {player_id}")
     is_near, is_hit, n_hits, is_dead, monsters_locations, data = game.is_hit(player_id)
     if is_near:
-        socketio.emit("hit_by_monsters", {"n_hits": n_hits,"is_hit": is_hit, "monsters_locations": monsters_locations}, to=player_id)
+        socketio.emit("hit_by_monsters", {"n_hits": n_hits,"is_hit": is_hit, "monsters_locations": monsters_locations}, to=player_index[player_id])
         if is_dead:
             print(f"GAME OVER for the player {player_id}")
-            socketio.emit("game_over", to=player_id)
+            socketio.emit("game_over", to=player_index[player_id])
             game.removePlayer(player_id)
             socketio.emit("a player died", data, room=room)
 
@@ -81,26 +108,36 @@ def on_is_hit_msg(json):
 @socketio.on("shoot")
 def on_shoot_msg(json):
     room = json["room"]
-    player_id = json["id"]
+    socket_id = json["id"]
+    player_id = json["player_id"]
+    check_id(player_id, socket_id)
     game = game_index[room]
     print(f"received shoot message from {player_id} ")
     hit, hit_player, is_dead, data = game.hitOpponent(player_id)
     if hit:
-        socketio.emit("shoot result", "you got it", to=player_id)
+        socketio.emit("shoot result", "you got it", to=player_index[player_id])
         socketio.emit("you_got_shot",  to=hit_player)
         if is_dead:
             print(f"GAME OVER for the player {hit_player}")
             socketio.emit("game_over", to=hit_player)
             socketio.emit("a player died", data)
+            game.removePlayer(hit_player)
     else:
-        socketio.emit("shoot result", "you missed", to=player_id)
+        socketio.emit("shoot result", "you missed", to=player_index[player_id])
 
-@socketio.on("disconnect")
+@socketio.on("disconnected")
 def on_disconnect_msg(json):
     room = json["room"]
-    player_id = json["id"]
+    player_id = json["player_id"]
     print(f"received disconnect message from {player_id} ")
     leave_room(room)
+
+def check_id(player_id, socket_id):
+    if not(player_id in player_index):
+        raise KeyError
+    if player_index[player_id] != socket_id:
+        player_index[player_id] = socket_id
+
 
 if __name__=="__main__":
     socketio.run(app, port=5001)
--- static/js/main.js
+++ static/js/main.js
@@ -6,28 +6,58 @@ window.addEventListener("DOMContentLoaded", (event) => {
     var message = document.getElementById("message");
     message.innerHTML = `press enter to join the game`;
     var room = document.getElementById("game_id").innerText;
+    var player_id = getCookie("player_id");
+    var multi = true;
+    
+    
 
+    var save_game_button = document.getElementById("save_game")
+    if (save_game_button !=null){
+        multi = false;
+        save_game_button.onclick = function() {
+            setCookie("game_id", `${room}`);
+        }
+    }
+    var new_level_button = document.getElementById("new_level")
+    if (new_level_button !=null){
+        new_level_button.onclick = function() {
+            document.cookie = 'game_id=; Max-Age=-99999999;'
+            socket.emit("disconnected", {id: socket.id, room:room, player_id:player_id})
+            socket.disconnect();
+        }
+    }
 
     document.onkeydown = function(e){
         switch(e.keyCode){
             case 37:
-                socket.emit("move", {dx:-1, dy:0, id: socket.id, room:room});
+                socket.emit("move", {dx:-1, dy:0, id: socket.id, room:room, player_id:player_id});
                 break;
             case 38:
-                socket.emit("move", {dx:0, dy:-1, id: socket.id, room:room});
+                socket.emit("move", {dx:0, dy:-1, id: socket.id, room:room, player_id:player_id});
                 break;
             case 39:
-                socket.emit("move", {dx:1, dy:0, id: socket.id, room:room});
+                socket.emit("move", {dx:1, dy:0, id: socket.id, room:room, player_id:player_id});
                 break;
             case 40:
-                socket.emit("move", {dx:0, dy:1, id: socket.id, room:room});
+                socket.emit("move", {dx:0, dy:1, id: socket.id, room:room, player_id:player_id});
                 break;
             case 13:
-                socket.emit("join", {id: socket.id, room:room});
+                if (player_id == null){
+                    setCookie("player_id", socket.id, 7);
+                    player_id = getCookie("player_id");
+                    socket.emit("join", {id: socket.id, room:room, player_id:player_id});
+                }
+                else if(multi){
+                    socket.emit("join", {id: socket.id, room:room, player_id:player_id});
+                }
+                else{
+                    socket.emit("get a saved game", {id: socket.id, room:room, player_id:player_id});
+                }
                 break;
             case 32:
-                socket.emit("shoot", {id: socket.id, room:room});
+                socket.emit("shoot", {id: socket.id, room:room, player_id:player_id});
                 console.log("shoot");
+                console.log(document.cookie);
                 break;
         }
 
@@ -37,25 +67,25 @@ window.addEventListener("DOMContentLoaded", (event) => {
     var btn_n = document.getElementById("go_n");
     btn_n.onclick = function(e) {
         console.log("Clicked on button north");
-        socket.emit("move", {dx:0, dy:-1, id:socket.id, room:room});
+        socket.emit("move", {dx:0, dy:-1, id: socket.id, room:room, player_id:player_id});
     };
 
     var btn_s = document.getElementById("go_s");
     btn_s.onclick = function(e) {
         console.log("Clicked on button south");
-        socket.emit("move", {dx:0, dy:1, id:socket.id, room:room});
+        socket.emit("move", {dx:0, dy:1, id: socket.id, room:room, player_id:player_id});
     };
 
     var btn_w = document.getElementById("go_w");
     btn_w.onclick = function(e) {
         console.log("Clicked on button w");
-        socket.emit("move", {dx:-1, dy:0, id:socket.id, room:room});
+        socket.emit("move", {dx:-1, dy:0, id: socket.id, room:room, player_id:player_id});
     };
 
     var btn_e = document.getElementById("go_e");
     btn_e.onclick = function(e) {
         console.log("Clicked on button e");
-        socket.emit("move", {dx:1, dy:0, id:socket.id, room:room});
+        socket.emit("move", {dx:1, dy:0, id: socket.id, room:room, player_id:player_id});
     };
 
     socket.on("you_joined", function(data){
@@ -96,7 +126,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
             message.innerHTML = "congratuations you got an extra life !";
             clean(500);
         }
-        socket.emit("is_hit?", {id: socket.id, room:room});
+        socket.emit("is_hit?", {id: socket.id, room:room, player_id:player_id});
     });
 
     socket.on("hit_by_monsters", async function(data){
@@ -119,7 +149,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         }
         await new Promise(resolve => setTimeout(resolve, 500));
         console.log({id: socket.id});
-        socket.emit("is_hit?", {id: socket.id, room:room});
+        socket.emit("is_hit?", {id: socket.id, room:room, player_id:player_id});
     });
 
     socket.on("shoot result", function(response){
@@ -148,10 +178,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
     } )
     socket.on("game_over", function(){
         console.log("game_over");
-        socket.emit("disconnect", {id: socket.id, room:room})
+        socket.emit("disconnected", {id: socket.id, room:room, player_id:player_id})
         socket.disconnect();
         message.innerHTML = "GAME OVER"
     } )
+    
 
     async function clean(ms){
         await new Promise(resolve => setTimeout(resolve, ms));
@@ -159,4 +190,25 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
     }
 
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+            for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            }
+        return null;
+        }
+
+    function setCookie(name,value,days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days*24*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "")  + ";expires=Sat," + expires ;
+    }
+    
 });
